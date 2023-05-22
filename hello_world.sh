8d6ac6b4505e46a13dd256aea1a8e27b86298aee
DungQuack <helo@DESKTOP-TON2QH1.localdomain>
2023-05-22 12:52:17 +0700

    Add bash trap example

--- hello_world.sh
+++ hello_world.sh
@@ -3,3 +3,16 @@
 STRING="Hello World, this is DungQuach"
 # print variable on a screen
 echo $STRING
+echo -e "--------------------------------"
+echo -e "Hi, this is Dung ,who are you ?"
+read word1 word2
+echo "Oh hello $word1 nice to meet you too $word2 !"
+echo "How do you feel right now?"
+read
+echo "$REPLY, I glad to hear that !"
+echo "$word1 can i ask you a question ?"
+read
+echo -e "What are your 3 favorite colours ?"
+read -a colours
+echo "You like ${colours[1]} too!, I love that colour."
+
