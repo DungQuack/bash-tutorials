ec034e27fda0841d6a3095754d64c84b022ebe43
DungQuack <helo@DESKTOP-TON2QH1.localdomain>
2023-05-22 12:58:26 +0700

    Second pushing bash_trapCommand

--- bash_trapCommand.sh
+++ bash_trapCommand.sh
@@ -2,12 +2,11 @@
 # bash trap command
 trap bashtrap INT
 # bash clear screen command
-clear;
 # bash trap function is executed when CTRL-C is pressed:
 # bash prints message => Executing bash trap subrutine !
 bashtrap()
 {
-	echo "CTRL+C Detected !...executing bash trap !"
+	echo " Hi there beauty! <3"
 }
 # for loop from 1/10 to 10/10
 for a in `seq 1 10`; do
